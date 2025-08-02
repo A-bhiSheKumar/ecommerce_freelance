12c7b9b0b6656d27a8200ac2c72822729fa89691
Abhishek Kumar <134307387+A-bhiSheKumar@users.noreply.github.com>
2025-08-02 22:26:57 +0530

    feat: updated with the protected route

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
 import LoginForm from "./components/LoginForm";
 import { Toaster } from "react-hot-toast";
 import HomeLayout from "./components/HomeLayout";
+import ProtectedRoute from "./shared/ProtectedRoute";
 
 const App = () => {
   return (
@@ -11,7 +11,14 @@ const App = () => {
       <Router>
         <Routes>
           <Route path="/" element={<LoginForm />} />
-          <Route path="/home/*" element={<HomeLayout />} />
+          <Route
+            path="/home/*"
+            element={
+              <ProtectedRoute>
+                <HomeLayout />
+              </ProtectedRoute>
+            }
+          />
         </Routes>
       </Router>
     </>
